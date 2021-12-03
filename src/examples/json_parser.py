72bd3c88fd21f3609f8c07f87a7cdf29c2dc7462
sayampy <sujata.howrah.belgachia@gmail.com>
2021-12-03 15:42:26 +0530

    Released

--- src/examples/json_parser.py
+++
@@ -1,6 +0,0 @@
-from textparser import Parser,re
-parser = Parser(line_sep=r'[\{\[].*[\}|\]](?:"|[^"])*',flags=re.MULTILINE) #default line_sep='\n'
-
-
-@parser._(r'(?:"|[^"])*')
-def parse_dict()
--- src/examples/variable_format.py
+++
@@ -1,2 +0,0 @@
-from ..textparser import Parser
-
